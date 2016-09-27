3a8b4f75c937e53add4b9e19e084b8d60ba64be7
tianez <saber_tz@163.com>
2016-09-28 00:24:54 +0800

    1

--- app.js
+++ app.js
@@ -28,6 +28,39 @@ app.on('error', function(err, ctx) {
     console.log(err);
 });
 
+
+
 const server = app.listen(3000, function() {
     console.log('Koa is listening to http://localhost:3000');
 });
+
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+let wss = new WebSocketServer({
+    server: server
+});
+
+wss.on('connection', function(ws) {
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function(message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    })
+});
+
+let ws = new WebSocket('ws://localhost:3000/test');
+
+// 打开WebSocket连接后立刻发送一条消息:
+ws.on('open', function() {
+    console.log(`[CLIENT] open()`);
+    ws.send('Hello!');
+});
+
+// 响应收到的消息:
+ws.on('message', function(message) {
+    console.log(`[CLIENT] Received: ${message}`);
+})
