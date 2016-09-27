62de9483600983c371df25c671af553664366483
tianez <saber_tz@163.com>
2016-09-27 20:36:42 +0800

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = new Koa();
 
 var router = require('koa-router')();
 
-router.get('/', function* (next) {
+router.get('/', function*(next) {
     yield next
     console.log('1wode');
     this.body = 'Hello World';
@@ -17,12 +17,11 @@ app.use(function* chat(next) {
     console.log('123232222');
 })
 
-router.get('/dd', function* (next) {
+router.get('/dd', function*(next) {
     this.body = 'Hello World2';
 });
 app.use(router.routes())
 app.use(router.allowedMethods());
-
 
 app.use(function* pageNotFound(next) {
     console.log('89');
