79bd24406caeaf2b388eb5f2b20416afb86cdeb0
tianez <saber_tz@163.com>
2016-10-17 23:55:18 +0800

    11111111111111

--- app/controllers/wechat.js
+++ app/controllers/wechat.js
@@ -8,16 +8,38 @@ const xml2js = require('xml2js');
 
 const fs = require('fs');
 
+const request = require('superagent')
+
 const config = {
     token: 'weixin',
     appid: 'wxe99ca8d62941e691',
-    encodingAESKey: '42d505a91367458b3313082c8119af55'
+    secret: '42d505a91367458b3313082c8119af55',
+    wx_url: 'https://api.weixin.qq.com/cgi-bin/',
+    api_url: 'https://api.weixin.qq.com/',
+    token_url: 'token',  //获取token
+    ip_url: 'getcallbackip', //获取微信服务器IP地址
+    create_meun: 'menu/create', //创建自定义菜单
+    get_meun: 'menu/get', //获取自定义菜单
+    delete_meun: 'menu/delete', //删除自定义菜单
+    template: 'template/api_set_industry', //设置行业
 };
 const WechatAPI = require('wechat-api');
+const redis = require('../lib/redis')
 
 async function weixin(ctx, next) {
-    console.log(111);
-    ctx.body = 'sssss'
+
+    // let token = await gettoken()
+    let data = {
+        "query": "查一下明天从北京到上海的南航机票",
+        "city": "北京",
+        "category": "flight,hotel",
+        "appid": "wxaaaaaaaaaaaaaaaa",
+        "uid": "123456"
+    }
+    let res = await apipostfetch('semantic/semproxy/search')
+    ctx.body = res
+    // let ip = await getfetch(config.ip_url)
+    // console.log(ip);
     // let signature = ctx.query.signature
     // let timestamp = ctx.query.timestamp
     // let nonce = ctx.query.nonce
@@ -61,4 +83,121 @@ async function postweixin(ctx, next) {
 module.exports = {
     weixin: weixin,
     postweixin: postweixin,
+}
+
+async function getfetch(url, query) {
+    if (!query) {
+        query = {}
+    }
+    Object.assign(query, {
+        access_token: await redis.get('wxtoken')
+    })
+    return new Promise(function (resolve, reject) {
+        request
+            .get(config.wx_url + url)
+            .query(query)
+            .end(function (err, res) {
+                if (res.ok) {
+                    resolve(JSON.parse(res.text))
+                } else {
+                    reject(err.response);
+                }
+            })
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+async function postfetch(url, data, query) {
+    if (!query) {
+        query = {}
+    }
+    Object.assign(query, {
+        access_token: await redis.get('wxtoken')
+    })
+    return new Promise(function (resolve, reject) {
+        request
+            .post(config.wx_url + url)
+            .query(query)
+            .send(data)
+            .end(function (err, res) {
+                if (res.ok) {
+                    resolve(JSON.parse(res.text))
+                } else {
+                    reject(err.response);
+                }
+            })
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+async function apigetfetch(url, query) {
+    if (!query) {
+        query = {}
+    }
+    Object.assign(query, {
+        access_token: await redis.get('wxtoken')
+    })
+    return new Promise(function (resolve, reject) {
+        request
+            .get(config.api_url + url)
+            .query(query)
+            .end(function (err, res) {
+                if (res.ok) {
+                    resolve(JSON.parse(res.text))
+                } else {
+                    reject(err.response);
+                }
+            })
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+async function apipostfetch(url, data, query) {
+    if (!query) {
+        query = {}
+    }
+    Object.assign(query, {
+        access_token: await redis.get('wxtoken')
+    })
+    return new Promise(function (resolve, reject) {
+        request
+            .post(config.api_url + url)
+            .query(query)
+            .send(data)
+            .end(function (err, res) {
+                if (res.ok) {
+                    resolve(JSON.parse(res.text))
+                } else {
+                    reject(err.response);
+                }
+            })
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+async function gettoken() {
+    return new Promise(function (resolve, reject) {
+        request
+            .get(config.wx_url + 'token')
+            .query({
+                grant_type: 'client_credential',
+                appid: config.appid,
+                secret: config.secret
+            })
+            .end(function (err, res) {
+                if (res.ok) {
+                    let data = JSON.parse(res.text)
+                    redis.set('wxtoken', data.access_token, data.expires_in)
+                    resolve(data)
+                } else {
+                    reject(err.response);
+                }
+            })
+    }).catch(function (err) {
+        console.log(err);
+    });
 }
